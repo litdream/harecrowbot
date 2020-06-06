0f2877f03748230a0024d4ee8cea098e4d662bb0
Ubuntu <ubuntu@ip-172-31-44-12.ec2.internal>
2020-06-06 01:11:52 +0000

    author is not string.  It is type Member.

--- bot_hello.py
+++ bot_hello.py
@@ -6,7 +6,7 @@ class Hello:
         self.hito = ctx.message.author
         
     def hello(self):
-        greetuser = self.hito
+        greetuser = self.hito.__str__()
         if greetuser.find('#') > -1:
             greetuser = greetuser.split('#')[0]
         return "Hello, {}.".format(greetuser)
